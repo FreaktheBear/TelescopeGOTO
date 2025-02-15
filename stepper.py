6efab5bbf9079c7ef00c1994b1969285177724b3
FreaktheBear <freekdenbeer@hotmail.com>
2025-02-15 17:54:41 +1300

    Changed to use of 1 (one) stepper class. Changed calculation of steps and rotation direction when NexStar integer is 2**32/2 (12h/180Deg), still work in progress. Added Tracking Clock. Have to add UTC offset compensation for RA.

--- stepper.py
+++ stepper.py
@@ -1,37 +1,25 @@
 from machine import Pin, Timer
 import utime
-
-g_ms12_pin_2 = Pin(8, Pin.OUT, value=1)         # FdB: Defines Pin MS1_2 as step 1/8 pins for motor 2 (DEC)
-g_ms3_pin_2 = Pin(9, Pin.OUT, value=0)          # FdB: Defines Pin MS3 as step 1/16 pin
-
-class Stepper_DEC:
+ 
+class Stepper:
     def __init__(self, dir_pin, step_pin):
         self.dir_pin = Pin(dir_pin, Pin.OUT)
         self.step_pin = Pin(step_pin, Pin.OUT)
         self.position = 0
- 
+
     def move(self, steps, delay, accel):
-        min_delay = 100
         max_delay = delay
-        if steps > 0:
-            self.dir_pin.value(1)
-            self.position += steps
-        else:
-            self.dir_pin.value(0)
-            self.position -=steps
-
+        self.dir_pin.value(1 if steps > 0 else 0)
         steps = abs(steps)
-
         for i in range(steps):
             self.step_pin.value(1)
             utime.sleep_us(delay)
             self.step_pin.value(0)
             utime.sleep_us(delay)
-
-            if i < steps // 2 and delay > min_delay:
+            if i < steps // 2 and delay > 100:
                 delay -= accel
             elif i >= steps // 2 and delay < max_delay:
                 delay += accel
+        self.position += steps if steps > 0 else -steps
 
-if __name__ == '__main__':
-    loop()
+
